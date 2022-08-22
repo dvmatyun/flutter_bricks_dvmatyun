29c04ffe3df1303a73954f37c02946f4eedb1dec
dvmatyun <matiunin.dv@nsd.ru>
2022-08-22 23:08:56 +0400

    example added

--- bricks/overlay/__brick__/lib/ui/overlay/sliding_overlay/example.dart
+++ bricks/overlay/__brick__/lib/ui/overlay/sliding_overlay/example.dart
@@ -2,31 +2,11 @@ import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_bricks_dvmatyun/ui/overlay/common/models/overlay_params.dart';
-import 'package:flutter_bricks_dvmatyun/ui/overlay/common/models/typed_message.dart';
-import 'package:flutter_bricks_dvmatyun/ui/overlay/sliding_overlay/domain/controllers/top_message_notificator.dart';
-import 'package:flutter_bricks_dvmatyun/ui/overlay/sliding_overlay/presentation/controllers/top_message_notificator_impl.dart';
-import 'package:flutter_bricks_dvmatyun/ui/overlay/sliding_overlay/presentation/widgets/notification_message_widget.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+import 'package:{{packageName.snakeCase()}}/ui/overlay/common/models/overlay_params.dart';
+import 'package:{{packageName.snakeCase()}}/ui/overlay/common/models/typed_message.dart';
+import 'package:{{packageName.snakeCase()}}/ui/overlay/sliding_overlay/domain/controllers/top_message_notificator.dart';
+import 'package:{{packageName.snakeCase()}}/ui/overlay/sliding_overlay/presentation/controllers/top_message_notificator_impl.dart';
+import 'package:{{packageName.snakeCase()}}/ui/overlay/sliding_overlay/presentation/widgets/notification_message_widget.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
--- lib/ui/overlay/sliding_overlay/example.dart
+++ lib/ui/overlay/sliding_overlay/example.dart
@@ -8,26 +8,6 @@ import 'package:flutter_bricks_dvmatyun/ui/overlay/sliding_overlay/domain/contro
 import 'package:flutter_bricks_dvmatyun/ui/overlay/sliding_overlay/presentation/controllers/top_message_notificator_impl.dart';
 import 'package:flutter_bricks_dvmatyun/ui/overlay/sliding_overlay/presentation/widgets/notification_message_widget.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
